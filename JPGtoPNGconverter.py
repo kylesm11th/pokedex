9935d53904b38d80d32ae7d6175cff4150175862
kylesm11th <98784440+kylesm11th@users.noreply.github.com>
2022-01-31 23:58:01 +0000

    Update JPGtoPNGconverter.py

--- JPGtoPNGconverter.py
+++ JPGtoPNGconverter.py
@@ -2,16 +2,19 @@ import sys
 import os
 from PIL import Image
 
+    #grab the 1st and 2nd argument
 path = sys.argv[1]
 directory = sys.argv[2]
 
+    #check if new folder exists, if not, create it.
 if not os.path.exists(directory):
     os.makedirs(directory)
     
-
+    #loop through pokedox
 for filename in os.listdir(path):
   clean_name = os.path.splitext(filename)[0]
   img = Image.open(f'{path}{filename}')
-  #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
+    #added the / in case user doesn't enter it. You may want to check for this and add or remover it. 
+    #convert to png  
   img.save(f'{directory}/{clean_name}.png', 'png')
   print('all done!')
